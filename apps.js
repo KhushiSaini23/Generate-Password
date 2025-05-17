809a248d388bbd86334f481e674493a2dafb2c67
Khushi Saini <khushisaini2317@gmail.com>
2025-05-17 23:58:50 +0530

    Add files via upload

---
+++ apps.js
@@ -0,0 +1,44 @@
+     console.log("welcome to Random Generate")
+
+
+     const btn = document.querySelector("#generate");
+     const passwordBox = document.querySelector("#Password");
+     const copy = document.querySelector("#copy");
+
+
+
+     
+
+
+
+     const length = 12;
+
+     const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+     const lowerCase ="abcdefghijklmnopqrstuvwxyz";
+     const number = "0123456789";
+     const symbol ="@#$%^&*()_-+=~|<>/";
+
+      
+     const allChars = upperCase+lowerCase+number+symbol;
+
+      function createPassword(){
+        let password ="";
+        password +=upperCase[Math.floor(Math.random() * upperCase.length)];
+        password +=lowerCase[Math.floor(Math.random() * lowerCase.length)];
+        password +=number[Math.floor(Math.random() * number.length)];
+        password +=symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(password.length < length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+         
+    }
+    passwordBox.value = password;
+
+      }
+      
+     
+
+
+  btn.addEventListener("click",createPassword())
+ // copy.addEventListener("click",copyPassword)
+
